61666021357ddd5011a2b5ae1c16e488e97cbaf6
lanou3g <285822781@qq.com>
2016-03-17 09:39:32 +0800

    第一次提交

---
+++ star-/star-/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  star-
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
